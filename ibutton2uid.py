3f83d1f8b51051bffff3449e97820687598cdedf
James Forcier <csssuf@csssuf.net>
2016-12-03 23:58:01 -0500

    Initial code commit

---
+++ ibutton2uid.py
@@ -0,0 +1,6 @@
+import requests
+def ibutton2uid(ibutton, url="http://ibutton2uid.app.csh.rit.edu/"):
+    r = requests.get(url + ibutton)
+    if r.status_code == 200:
+        return r.text
+    return None
